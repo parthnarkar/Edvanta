367936a6bff0e0b3fced9feef627fd184673583b
Tanish Jain <tanishjain020205@gmail.com>
2025-09-19 21:39:34 +0530

    CORS Test

--- client/src/hooks/helper.js
+++ client/src/hooks/helper.js
@@ -1,5 +1,5 @@
 let url = ''
-if (import.meta.env.MODE === 'production') {
+if (import.meta.env.VITE_MODE == "production") {
   url = import.meta.env.VITE_PRODUCTION_API_BASE_URL
 } else {
   url = import.meta.env.VITE_API_BASE_URL
